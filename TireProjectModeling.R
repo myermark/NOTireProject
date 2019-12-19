b76f707c976755d6b5fe6fcd8835f568e5378490
Mark Myer <myermarkh@gmail.com>
2019-12-19 09:10:25 -0600

    Map sampling points

--- TireProjectModeling.R
+++ TireProjectModeling.R
@@ -10,4 +10,21 @@
 # R version 3.6.1 Action of the Toes
 ####---
 
+#Load packages
+library(INLA)
+library(rgdal)
+library(sf)
+library(ggmap)
+library(dplyr)
+library(raster)
 
+#Import data 
+dat.selected <- readRDS(file = "Mosquito_Variables_Selected.rds")
+
+#Plot the sampling locations
+border <- readOGR("/Volumes/Mark Drive/NOLA/GIS/New Orleans/CityLimits.shp")
+border <- spTransform(border, crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) #reproject to decimal degrees in WGS84
+
+#Visualize using ggmaps
+nola_stamen <- get_stamenmap(bbox = border@bbox, zoom = 11, maptype = "terrain")
+map <- ggmap(nola_stamen)
